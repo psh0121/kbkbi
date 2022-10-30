2a260d4ae62d43d266331b6147bdc85f4b121806
psh0121 <tngus2057@gmail.com>
2022-10-30 21:49:12 +0900

    create file Equals

---
+++ codingPractice/APIProblem/Equals/src/Member.java
@@ -0,0 +1,29 @@
+
+public class Member {
+	private String id;
+	
+	public Member()
+	{
+		this.id = null;
+	}
+	
+	public Member(String id)
+	{
+		this.id = id;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj instanceof Member)
+		{
+			Member member = (Member) obj;
+			
+			if(id.equals(member.id))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
