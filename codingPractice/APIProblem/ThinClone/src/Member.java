c2b6564597601746ce8c110f58b625a60e151695
psh0121 <tngus2057@gmail.com>
2022-11-01 00:35:41 +0900

    Modify file ThineClone (얕은복제)

--- codingPractice/APIProblem/ThinClone/src/Member.java
+++ codingPractice/APIProblem/ThinClone/src/Member.java
@@ -1,4 +1,67 @@
 
-public class Member {
+public class Member implements Cloneable{
+	private String id;
+	private String name;
+	private String password;
+	private int age;
+	private boolean adult;
+	
+	public Member()
+	{
+		id = null;
+		name = null;
+		password = null;
+		age = 0;
+		adult = false;
+	}
+	
+	public Member (String id, String name, String password, int age, boolean adult)
+	{
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		this.adult = adult;
+	}
+	
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	
+	public Member getMember()
+	{
+		Member cloned = null;
+		
+		try
+		{
+			cloned = (Member) clone();
+		}
+		
+		catch(CloneNotSupportedException e) {}
+		
+		return cloned;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public boolean getAdult() {
+		return adult;
+	}
+	
+	
 }
--- codingPractice/APIProblem/ThinClone/src/MemberExample.java
+++ codingPractice/APIProblem/ThinClone/src/MemberExample.java
@@ -2,7 +2,28 @@
 public class MemberExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 원본 객체 생성
+		Member original = new Member("blue", "홍길동", "12345", 25, true);
+		
+		// 복제 객체를 얻은 후에 패스워드 변경 
+		Member cloned = original.getMember();
+		cloned.setPassword("67890");
+		
+		System.out.println("[복제 객체의 필드값]");
+		System.out.println("id: " + cloned.getId());
+		System.out.println("name: " + cloned.getName());
+		System.out.println("password: " + cloned.getPassword());
+		System.out.println("age: " + cloned.getPassword());
+		System.out.println("adult: " + cloned.getAdult());
+		
+		System.out.println();
+		
+		System.out.println("[원본 객체의 필드값]");
+		System.out.println("id: " + original.getId());
+		System.out.println("name: " + original.getName());
+		System.out.println("password: " + original.getPassword());
+		System.out.println("age: " + original.getPassword());
+		System.out.println("adult: " + original.getAdult());
 
 	}
 
